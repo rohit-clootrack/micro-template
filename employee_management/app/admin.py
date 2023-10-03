2a16f14ddfaa0e474203449fcdd74d842a0a9655
Rohit Agarwal <rohit.agarwal@clootrack.com>
2023-10-03 19:50:22 +0530

    Update Admin page

--- employee_management/app/admin.py
+++ employee_management/app/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from employee_management.app.models.employee import Department, Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
