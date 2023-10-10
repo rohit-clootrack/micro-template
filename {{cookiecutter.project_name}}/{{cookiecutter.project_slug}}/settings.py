f37b052f4a3843d20a8f5f8d29224fe601db6940
Rohit Agarwal <rohit.agarwal@clootrack.com>
2023-10-10 13:12:27 +0530

    Fix: add allowed_hosts

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-k3*t!##y&yqpejdvpzm*q)*=h#g!a+h@6r2t6xx(wfufon+rp*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 # Application definition
 
