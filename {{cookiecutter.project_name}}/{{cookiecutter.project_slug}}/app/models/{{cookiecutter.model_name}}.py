c1de852390bbc31e6adf696b2731279452d4a830
Rohit Agarwal <rohit.agarwal@clootrack.com>
2023-11-01 18:22:23 +0530

    Add MetaData model

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/app/models/{{cookiecutter.model_name}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/app/models/{{cookiecutter.model_name}}.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 
-class {{cookiecutter.model_name | title}}(models.Model):
+class MetadataModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_by = models.UUIDField(blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, db_index=True)
+    updated_by = models.UUIDField(blank=True, null=True)
+    deleted_on = models.DateTimeField(blank=True, null=True, db_index=True)
+    deleted_by = models.UUIDField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class {{cookiecutter.model_name | title}}(MetadataModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
